e8cfb17824a9c54cae9136bef2dcfd492b52abfc
mbarz6 <mbarz6@users.noreply.github.com>
2017-10-10 14:08:37 -0500

    Update exercise1.py

--- day2/exercise1.py
+++ day2/exercise1.py
@@ -1,1 +1,14 @@
+dogs = ["jack", "chrissy"]
+people = ["michael", "matthew", "anton", "aayushi", "molly", "sparks", "justin"]
+test_scores = [93, 99, 91, 75]
 
+print("Initial values: ")
+print(dogs)
+print(people)
+print(test_scores)
+
+# change the lists
+
+# print the new values of the lists
+
+print("Modified values: ")
