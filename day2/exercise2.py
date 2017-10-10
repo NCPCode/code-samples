ed2ad825e00752df9e2c89609d8bc79ccf55199d
mbarz6 <mbarz6@users.noreply.github.com>
2017-10-10 14:11:40 -0500

    Create exercise2.py

---
+++ day2/exercise2.py
@@ -0,0 +1,10 @@
+# ask a user how many favorite animals they have
+# then, get all their favorite animals and store them in a list
+# finally, print out that list of their favorite animals
+
+# for example, your program might run like this: (user input is in parentheses)
+# How many favorite animals do you have? (3)
+# What is favorite animal #1? (dog)
+# What is favorite animal #2? (dolphin)
+# What is favorite animal #3? (kangaroo)
+# You like dogs, dolphins, and kangaroos!
