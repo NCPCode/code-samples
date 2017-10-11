361dd2f67174fffcd629d68d2ed210dabbeaba25
Jetmate <antonoutkine@gmail.com>
2017-10-11 07:04:39 -0500

    Add content for day2

--- day2/exercise1.py
+++ day2/exercise1.py
@@ -7,8 +7,30 @@ print(dogs)
 print(people)
 print(test_scores)
 
-# change the lists
+# change the lists so that...
+# dogs = ["jack", "chrissy", "pixel", "byte"]
+# people = ["matthew", "aayushi", "molly", "sparks"]
+# test_scores = [93]
 
-# print the new values of the lists
 
-print("Modified values: ")
+
+
+
+# now let's check!
+
+if dogs == ["jack", "chrissy", "pixel", "byte"]:
+    print('list 1: success!')
+else:
+    print('FAILED list 1 test')
+
+
+if people == ["matthew", "aayushi", "molly", "sparks"]:
+    print('list 2: success!')
+else:
+    print('FAILED list 2 test')
+
+
+if test_scores == [93]:
+    print('list 3: success!')
+else:
+    print('FAILED list 3 test')
