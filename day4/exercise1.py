c83cb0ad4ca0e4bfba254ee78758ac55e279c523
mbarz6 <mbarz6@users.noreply.github.com>
2017-10-24 11:14:15 -0500

    Create exercise1.py

---
+++ day4/exercise1.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+def draw_shape(sides, length):
+  # add some code which makes the turtle draw a shape with the given number of sides
+  # for example, draw_shape(3, 10) would draw a triangle with sides of length 10
+  # draw_shape(5, 6) would draw a pentagon with sides of length 6
+  
+# add something to test your code
