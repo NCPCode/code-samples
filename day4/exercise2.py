2b943d4e8c38f2f54cd5c1a39ba5e38269e6407c
mbarz6 <mbarz6@users.noreply.github.com>
2017-10-25 11:02:50 -0500

    Update exercise2.py

--- day4/exercise2.py
+++ day4/exercise2.py
@@ -1,8 +1,17 @@
-def absolute_difference(x, y):
-  # compute |x-y|
+def average(numbers):
+  # compute the average of that list of numbers
+  # remember to return the average!
+
+def absolute(x):
+  # compute |x|
   # remember to make return your answer!
 
 
-print(absolute_difference(5, 7))
-print(absolute_difference(7, 5))
-print(absolute_difference(6, 6))
+# tests!  
+  
+print(average([1, 2, 3, 4]) # should print 2.5
+print(average([0, 2, 4])    # should print 2 
+   
+print(absolute(5))          # should print 5
+print(absolute(-7))         # should print 7
+print(absolute(0))          # should print 0
