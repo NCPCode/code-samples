45d1f8e5f47d6176523558c6683560f0067f85d8
Jetmate <antonoutkine@gmail.com>
2017-10-25 11:03:11 -0500

    Remove answers from day3 exercises

--- day3/exercise1.py
+++ day3/exercise1.py
@@ -12,16 +12,6 @@ print(test_scores)
 # people = ["matthew", "aayushi", "molly", "sparks"]
 # test_scores = [93]
 
-dogs.append('pixel')
-dogs.append('byte')
-
-people.remove('anton')
-people.remove('michael')
-people.remove('justin')
-
-test_scores = test_scores[:1]
-
-
 # now let's check!
 
 if dogs == ["jack", "chrissy", "pixel", "byte"]:
