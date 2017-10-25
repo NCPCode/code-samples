5a51f79dc71dd2697248de7c174ca1e8254e81e7
mbarz6 <mbarz6@users.noreply.github.com>
2017-10-25 10:10:04 -0500

    Create exercise2.py

---
+++ day4/exercise2.py
@@ -0,0 +1,8 @@
+def absolute_difference(x, y):
+  # compute |x-y|
+  # remember to make return your answer!
+
+
+print(absolute_difference(5, 7))
+print(absolute_difference(7, 5))
+print(absolute_difference(6, 6))
